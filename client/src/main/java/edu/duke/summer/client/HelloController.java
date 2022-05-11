86348ecc61a30908f18459e332b01efa0bf4180e
Yuxuan Yang <yy340@duke.edu>
2022-05-11 11:36:34 -0400

    gradle configuration

---
+++ client/src/main/java/edu/duke/summer/client/HelloController.java
@@ -0,0 +1,16 @@
+package edu.duke.summer.client;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
+
