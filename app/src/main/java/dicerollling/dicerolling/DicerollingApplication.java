a4f6d5f055105ce42b15f5f6a00450ae5414608e
Yuxuan Yang <yy340@duke.edu>
2022-05-11 11:16:49 -0400

    gradle configuration

---
+++ app/src/main/java/dicerollling/dicerolling/DicerollingApplication.java
@@ -0,0 +1,13 @@
+package dicerollling.dicerolling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DicerollingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicerollingApplication.class, args);
+	}
+}
