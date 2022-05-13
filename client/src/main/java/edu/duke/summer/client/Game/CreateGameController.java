6ef23e2df54f0fd95f7862bd0bd6236bfefa151e
ql110 <ql110@duke.edu>
2022-05-13 12:33:04 -0400

    create_choose_game

---
+++ client/src/main/java/edu/duke/summer/client/Game/CreateGameController.java
@@ -0,0 +1,25 @@
+package edu.duke.summer.client.Game;
+
+//@Controller
+//@RestController
+//public class CreateGameController {
+
+//    @RequestMapping("/gameCenter")
+//    public String greeting(){
+//        return "game.html";
+//    }
+//
+//    @RequestMapping(value = "/gameCenter/index", method = RequestMethod.GET)
+//    public String createLoginForm(HttpServletResponse response){
+//        response.setHeader("Content-Type","text/html");
+//        return "index.html";
+//    }
+
+//    @GetMapping("/gameCenter/creatGame")
+//    public String showGameCreateForm(Model model){
+//        model.addAttribute("newGame", new Game());
+//        return "creatGame.html";
+//    }
+//
+//
+//}
