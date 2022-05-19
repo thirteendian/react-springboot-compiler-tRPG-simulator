ee9cf3c0dd4505984bd2210f6b49af78e9f80f09
NathanGao97 <gaozedong1111@gmail.com>
2022-05-18 22:30:20 -0400

    game service

---
+++ client/src/main/java/edu/duke/summer/client/service/GameService.java
@@ -0,0 +1,45 @@
+package edu.duke.summer.client.service;
+
+import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import javax.transaction.Transactional;
+
+import edu.duke.summer.client.database.model.Game;
+import edu.duke.summer.client.database.repository.GameRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.maxmind.geoip2.DatabaseReader;
+
+@Service
+@Transactional
+public interface GameService {
+  
+  Game createNewGame(final GameDto gameCreateDto) {}
+
+  List<Game> filterGame(final GameFilterDto gameFilterDto) {}
+
+  Game joinGame(final GameDto gameJoinDto) {}
+
+  boolean checkPassowrd(final String password) {}
+  
+  void saveGame(final Game game) {}
+
+  void deleteGame(final Game game) {}
+    
+}
