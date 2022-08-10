802fd84c687cdd7cb5659285aa5066be7c00e74a
NathanGao97 <gaozedong1111@gmail.com>
2022-08-10 19:42:04 -0400

    add function related interfaces

---
+++ server/src/main/java/edu/duke/summer/server/database/model/GameCode.java
@@ -0,0 +1,53 @@
+package edu.duke.summer.server.database.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+import java.lang.*;
+
+@Entity
+@Table(name = "gameCode")
+@IdClass(GameCode.class)
+public class GameCode implements Serializable {
+
+    @Id
+    @Column(nullable = false)
+    private String gameId;
+
+    @Id
+    @Column(nullable = false)
+    private String code;
+
+    public void setGameId(String gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getGameId() {
+        return gameId;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public GameCode() {}
+
+    public GameCode(String gameId, String code) {
+        this.gameId = gameId;
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return "GameCode{" +
+                "gameId=" + gameId +
+                ", code='" + code + '\'' +
+                '}';
+    }
+}
