27ce6ad8385d31f94c1263033e3f914544f3df0c
Yuxuan Yang <yy340@duke.edu>
2022-08-12 14:03:55 -0400

    React Client 7

--- server/src/main/java/edu/duke/summer/server/ClientApplication.java
+++ server/src/main/java/edu/duke/summer/server/ClientApplication.java
@@ -1,5 +1,7 @@
 package edu.duke.summer.server;
 
+import edu.duke.summer.server.database.model.MongoDBTest;
+import edu.duke.summer.server.database.repository.MyMongoRepository;
 import edu.duke.summer.server.database.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,6 +16,7 @@ import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
+@EnableMongoRepositories(basePackageClasses = MyMongoRepository.class)
 public class ClientApplication {
 
 
