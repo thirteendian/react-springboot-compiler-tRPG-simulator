ce9d07ba1df037c894dc8b784edf9738f6b4862b
Yuxuan Yang <yy340@duke.edu>
2022-08-22 11:19:25 -0400

    modify GameService

--- client/src/main/react/edu/duke/summer/client/src/App.js
+++ client/src/main/react/edu/duke/summer/client/src/App.js
@@ -9,6 +9,7 @@ import CreateGame from "./assemblies/CreateGame";
 import CreateObject from "./assemblies/CreateObject";
 import GameCenter from "./assemblies/GameCenter";
 import WaitingRoom from "./assemblies/GameRoom";
+import JoinGame from "./assemblies/JoinGame";
 class App extends Component {
     state={
         isLogin: false
@@ -27,7 +28,7 @@ class App extends Component {
                             <Route path="/signup" element={<Signup/>}/>
                             <Route path="/user/index_after_login" element={<NavigationAfterLogin/>}/>
                             <Route path="/user/createGame" element={<CreateGame/>}/>
-                            <Route path="user/joinGame" element={}/>
+                            <Route path="user/joinGame" element={<JoinGame/>}/>
                             <Route path="/user/createObject" element={<CreateObject/>}/>
                             <Route path="/user/gameCenter" element={<GameCenter/>}/>
                             <Route path="/user/waitingRoom" element={<WaitingRoom/>}/>
