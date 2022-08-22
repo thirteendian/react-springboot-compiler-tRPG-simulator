90f45fd8955692852600a3d91384e987e89fc639
Yuxuan Yang <yy340@duke.edu>
2022-08-22 11:19:25 -0400

    modify GameService

--- client/src/main/react/edu/duke/summer/client/src/assemblies/CreateObject/index.jsx
+++ client/src/main/react/edu/duke/summer/client/src/assemblies/CreateObject/index.jsx
@@ -1,73 +1,80 @@
-import React, {Component, Fragment, useEffect, useState} from 'react';
+import React, {Component, Fragment, useCallback, useEffect, useState} from 'react';
 import "./index.css";
 import Header from "../../components/Header";
 import ObjectButton from "../../components/ObjectButton";
-import MyNavLink from "../../components/MyNavLink";
 import OnClickButton from "../../components/OnClickButton";
-import List from "../../components/List";
-import InputText from "../../components/InputText";
-import UserService from "../../services/user.service";
+import {nanoid} from "nanoid";
+import {useNavigate} from "react-router-dom";
+import InputCheckBox from "../../components/InputCheckBox";
 
 export default function Index() {
-
     const [objectInfo, setObjectInfo] = useState(JSON.parse(localStorage.getItem("objectInfo")));
     const [message, setMessage] = useState("");
-    const [which,setWhich] = useState(0);
-    const [one,setone]=useState(false);
-    const [two,settwo]=useState(false);
-    const [three,setthree]=useState(false);
+    const [currentField, setCurrectField] = useState([]);
+    const navigate = useNavigate();
+    useEffect(() => {
+        //set each object with id={uuid}
+        objectInfo.map((each) => {
+            each.id = nanoid().toString()
+        })
+    }, [])
 
-    // useEffect(()=>{
-    //
-    //     console.log(objectInformation)
-    // })
-    const test= ()=>{
-        console.log(objectInfo)
+
+    const objectButtonOnClick = (fields) => {
+        return () => {
+            setCurrectField(fields);
+            console.log(fields);
+        }
     }
-    const listItems = objectInfo.map((each)=>
-        <li>
-            <ObjectButton>{each.objectName}</ObjectButton>
+
+    const listItems = objectInfo.map((each) =>
+        <li key={nanoid()}>
+            <ObjectButton onClick={objectButtonOnClick(each.fields)}>{each.objectName}</ObjectButton>
         </li>
     );
-    const onOne=()=>{
-        setone(true);
-        settwo(false);
-        setthree(false)
+
+    const createNestedStructure = (each,nestedStructure) => {
+        const k = each.objectFieldTypeDto.k;
+        if (k==="option"){
+            nestedStructure.push(<InputCheckBox className={"inputCheckBox_objectCreate"}/>)
+        }
+       return nestedStructure
     }
-    const onTwo=()=>{
-        setone(false);
-        settwo(true);
-        setthree(false)
+    const listFields = currentField.map((each) => {
+            let nestedStructure = [<Header className={"header_fieldName"}>{each.fieldName}: </Header>];
+            nestedStructure = createNestedStructure(each,nestedStructure);
+            nestedStructure.push(<hr/>)
+            return <li key={each.id}>
+                {nestedStructure}
+            </li>
+        }
+    )
+
+    const onClickBack = () => {
+        navigate("/user/gameCenter");
     }
-    const onThree=()=>{
-        setone(false);
-        settwo(false);
-        setthree(true)
+    const onClickCreate = () => {
+
     }
 
     return (
         <div>
             <Header className={"header_dice_rolling_game"}>Create Object</Header>
-            {/*<div>*/}
-            {/*    <ul>*/}
-            {/*        {listItems}*/}
-            {/*    </ul>*/}
-            {/*</div>*/}
 
-            <OnClickButton className={"onClickButton_createObject1"} onClick={onOne}>newType</OnClickButton>
-            <OnClickButton className={"onClickButton_createObject2"} onClick={onTwo}>test</OnClickButton>
-            <OnClickButton className={"onClickButton_createObject3"} onClick={onThree}>rollwithmod</OnClickButton>
-            {/*<div className="objectGenerateBackground">*/}
-                {one && (
-                    <Fragment>
-                        <Header className={"header_newType1"}>a: </Header>
-                        <Header className={"header_newType2"}>b: </Header>
-                        <Header className={"header_newType3"}>c: </Header>
-                        <InputText className={""} placeholder={"Insert A"}/>
-                    </Fragment>
-                )}
-            {/*</div>*/}
+            <div className={"objectMenuBackground"}>
+                <ul>
+                    {listItems}
+                </ul>
+            </div>
 
+            <div className="objectGenerateBackground">
+                <ul>
+                    {listFields}
+                </ul>
+            </div>
+
+            <OnClickButton className={"onClickButton_game_right"} onClick={onClickCreate}>Create</OnClickButton>
+            <OnClickButton className={"onClickButton_game_left"} onClick={onClickBack}>Back</OnClickButton>
 
         </div>
 
